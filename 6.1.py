1776fff4f5ff4768df23944797039811b638ab93
Dmitry <myxahev2@gmail.com>
2021-01-28 21:10:18 +0500

    complete 6.1

--- 6.1.py
+++ 6.1.py
@@ -12,3 +12,26 @@
 Задачу можно усложнить, реализовав проверку порядка режимов.
 При его нарушении выводить соответствующее сообщение и завершать скрипт.
 '''
+
+from time import sleep
+
+
+class TrafficLight:
+    __color = ['Red', 'Yellow', 'Green']
+
+    def running(self):
+        i = 0
+        while i != 3:
+            print(TrafficLight.__color[i])
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(2)
+            elif i == 2:
+                sleep(5)
+                print('Go')
+            i += 1
+
+
+t = TrafficLight()
+t.running()
