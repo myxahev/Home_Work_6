435d13b3d17fdcb175df58c3da8952b648331ce2
Dmitry <myxahev2@gmail.com>
2021-01-28 21:54:36 +0500

    update 6.2

--- 6.2.py
+++ 6.2.py
@@ -13,11 +13,17 @@
 '''
 
 class Road:
-    __length = 10
-    __width = 5
-    def calculation(self):
-        weight = 5000
-        print(f'{int(Road.__length * Road.__width * weight)} T.')
 
-a = Road()
-a.calculation()
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self.weight = 25
+        self.height = 5
+
+    def asphalt_mass(self):
+        asphalt_mass = self._length * self._width * self.weight * self.height / 1000
+        print(f'Для покрытия всего дорожного полотна неободимо {round(asphalt_mass)} массы асфальта')
+
+
+r = Road(5000, 20)
+r.asphalt_mass()
